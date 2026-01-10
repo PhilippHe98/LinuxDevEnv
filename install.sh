140c4558bbd5f36fe26415729820aa3d358d7e96
Philipp Heringer <79371575+PhilippHe98@users.noreply.github.com>
2026-01-10 18:22:27 +0100

    Use sudo for installing lazygit

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 if [ "$EUID" -ne 0 ]; then
     SUDO='sudo'
@@ -16,7 +15,7 @@ $SUDO apt install -y tmux git curl zsh make gcc ripgrep unzip xclip neovim zoxid
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
-install lazygit -D -t /usr/local/bin/
+sudo install lazygit -D -t /usr/local/bin/
 rm lazygit
 rm lazygit.tar.gz
 echo "done"
