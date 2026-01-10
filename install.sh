d1fb4166a2e369108c1ab5f97793526bff3df859
Philipp Heringer <philipp.heringer@gmail.com>
2026-01-10 15:11:22 +0100

    zsh + weitere config

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing everything"
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt install -y tmux git curl zsh make gcc ripgrep unzip xclip neovim
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+install lazygit -D -t /usr/local/bin/
+rm lazygit
+rm lazygit.tar.gz
+echo "done"
+
+echo "Setting up Tmux..."
+mkdir -p ~/.tmux
+ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+echo "done."
+
+echo "Setting up zsh"
+ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+echo "done"
+
+echo "Setting up neovim"
+git clone https://github.com/PhilippHe98/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+echo "done"
+
+chsh -s $(which zsh)
+
