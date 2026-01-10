017d3d6bbee0a52b162188f17fe7d9251f696e11
Philipp Heringer <79371575+PhilippHe98@users.noreply.github.com>
2026-01-10 15:45:01 +0100

    Refactor install script to use SUDO variable

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+if [ "$EUID" -ne 0 ]; then
+    SUDO='sudo'
+else
+    SUDO=''
+fi
+
 echo "Installing everything"
-sudo apt update
-sudo apt install -y software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt install -y tmux git curl zsh make gcc ripgrep unzip xclip neovim
+$SUDO apt update
+$SUDO apt install -y software-properties-common
+$SUDO add-apt-repository ppa:neovim-ppa/unstable -y
+$SUDO apt install -y tmux git curl zsh make gcc ripgrep unzip xclip neovim
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -29,4 +35,3 @@ git clone https://github.com/PhilippHe98/kickstart.nvim.git "${XDG_CONFIG_HOME:-
 echo "done"
 
 chsh -s $(which zsh)
-
