761f8bee53033b7326f8b0f46279f227c7a7c340
Philipp Heringer <philipp.heringer@gmail.com>
2026-01-10 15:59:18 +0100

    more packages

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ echo "Installing everything"
 $SUDO apt update
 $SUDO apt install -y software-properties-common
 $SUDO add-apt-repository ppa:neovim-ppa/unstable -y
-$SUDO apt install -y tmux git curl zsh make gcc ripgrep unzip xclip neovim
+$SUDO apt install -y tmux git curl zsh make gcc ripgrep unzip xclip neovim zoxide fzf
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
